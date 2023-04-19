7227de9472aff1002267a76fa863d57ed7a2919d
tumbleboy <jarredosborne123@gmail.com>
2023-04-19 17:29:28 +0800

    added score algo into the main function, created loop for attempts/score checking

--- project.py
+++ project.py
@@ -12,19 +12,11 @@ def get_target_word():
     return random.choice(target_words)
 
 
-target_word = get_target_word()
-result = []
-print(target_word)
-
-
 # Print a greeting to the user
 def greeting():
-    print("***Welcome to WORDLE.***\n"
+    print("*** Welcome to WORDLE. ***\n"
           "You have 6 attempts to guess the 5 letter word.\n"
           "Good luck!")
-
-
-greeting()
 
 
 # Get input from user
@@ -32,13 +24,36 @@ def get_user_word():
     return input("What is the 5 letter word?: ")
 
 
-user_word = list(get_user_word())
+# Function that runs the sequence of instructions
+def main_game():
+    greeting()
+    attempts = 6
+    target_word = get_target_word()
+    print(target_word)
+    while True:
+        result = ["_"]*5
+        user_word = get_user_word()
+        # Score guess
+        for position in range(len(user_word)):
+            if user_word[position] == target_word[position]:
+                result[position] = target_word[position]
+            # If the letter is in the word but incorrect space
+            elif user_word[position] in target_word:
+                result[position] = "*"
+
+        # If the letter is not in the word
+        for position in range(len(user_word)):
+            if user_word[position] is not target_word[position]:
+                continue
+        # Checks if user has run out of attempts or the word has been guessed.
+        if attempts == 0:
+            print("You lose")
+            break
+        elif target_word == user_word:
+            print("Congrats you guessed the word! it was", "".join(target_word).upper())
+            break
+        print(result)
+        attempts = attempts - 1
 
 
-# Compare each character in user word with the target word
-def score_guess():
-    pass
-
-
-score_guess()
-print(result)
+main_game()
